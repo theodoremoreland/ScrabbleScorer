80b6d07710ec79702f616d629920115df612835a
theodoremoreland <theodoremoreland@yahoo.com>
2024-02-01 20:20:03 -0600

    deleted unnecessary module

--- modules/scoringAlgorithms.js
+++ modules/scoringAlgorithms.js
@@ -1,4 +1,13 @@
-import "./String.js";
+String.prototype.isVowel = function () {
+  let vowels = ["a", "e", "i", "o", "u"];
+  if (vowels.includes(this[0].toLowerCase())) {
+    return 3;
+  } else if (this[0] === " ") {
+    return 0;
+  } else {
+    return 1;
+  }
+};
 
 const oldScoreKey = {
   1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
