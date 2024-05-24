e5041359ea118e52b7ffee081893cbb6e28d457c
theodoremoreland <theodoremoreland@yahoo.com>
2024-05-24 09:16:04 -0500

    updated front-end scaffolding

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// React
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Components
+import App from './App.tsx';
+
+// Styles
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
