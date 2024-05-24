9f2642b5d25d43b6bba995ab7650726d8ec985bf
theodoremoreland <theodoremoreland@yahoo.com>
2024-05-24 09:07:53 -0500

    added scaffolding for server file

---
+++ back-end/server.ts
@@ -0,0 +1,27 @@
+// Native modules
+import http from "http";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Third-party
+import express from "express";
+
+const port = process?.env?.PORT ? Number(process.env.PORT) : 8080;
+
+// Server initialization
+const app = express();
+const server = http.createServer(app);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middleware
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (_, res) => {
+  res.sendFile("index.html");
+});
+
+server.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}...`);
+});
