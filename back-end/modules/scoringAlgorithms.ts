ccb8a477109d8a72fe082ea18c36c9570fe5c6ee
theodoremoreland <theodoremoreland@yahoo.com>
2024-05-25 12:03:50 -0500

    refactor scoringAlgorithm.ts

--- back-end/modules/scoringAlgorithms.ts
+++ back-end/modules/scoringAlgorithms.ts
@@ -1,27 +1,19 @@
-const isVowel = (char: string) => {
-  let vowels = ["a", "e", "i", "o", "u"];
-
-  if (vowels.includes(char.toLowerCase())) {
-    return 3;
-  } else if (char === " ") {
-    return 0;
-  } else {
-    return 1;
-  }
+type OldScoreKey = {
+  [key: number]: string[];
 };
 
-const oldScoreKey = {
-  1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-  2: ["D", "G"],
-  3: ["B", "C", "M", "P"],
-  4: ["F", "H", "V", "W", "Y"],
-  5: ["K"],
-  8: ["J", "X"],
-  10: ["Q", "Z"],
+type NewScoreKey = {
+  [key: string]: number;
 };
 
-function transform(oldScoreKey) {
-  let newScoreKey = {};
+type ScoringAlgorithm = {
+  name: string;
+  description: string;
+  scoreFunction: (word: string) => number;
+};
+
+const transform = (oldScoreKey: OldScoreKey): NewScoreKey => {
+  const newScoreKey = {};
 
   for (let key in oldScoreKey) {
     for (let j = 0; j < oldScoreKey[key].length; j++) {
@@ -34,38 +26,66 @@ function transform(oldScoreKey) {
   newScoreKey[" "] = 0;
 
   return newScoreKey;
-}
+};
 
-const newScoreKey = transform(oldScoreKey);
+const oldScoreKey: OldScoreKey = {
+  1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+  2: ["D", "G"],
+  3: ["B", "C", "M", "P"],
+  4: ["F", "H", "V", "W", "Y"],
+  5: ["K"],
+  8: ["J", "X"],
+  10: ["Q", "Z"],
+};
 
-export const scoringAlgorithms: { name: string, description: string, scoreFunction: (word: string) => number }[] = [
-  {
-    name: "Scrabble",
-    description: "The traditional scoring algorithm.",
-    scoreFunction: (word: string): number => {
-      let points: number = 0;
+const newScoreKey: NewScoreKey = transform(oldScoreKey);
 
-      word
-        .split("")
-        .forEach((letter) => (points += newScoreKey[letter.toLowerCase()]));
+const scoreVowel = (char: string): 0 | 1 | 3 => {
+  let vowels = ["a", "e", "i", "o", "u"];
 
-      return points;
-    },
+  if (vowels.includes(char.toLowerCase())) {
+    return 3;
+  } else if (char === " ") {
+    return 0;
+  } else {
+    return 1;
+  }
+};
+
+const scrabbleAlgorithm: ScoringAlgorithm = {
+  name: "Scrabble",
+  description: "The traditional scoring algorithm.",
+  scoreFunction: (word: string): number => {
+    let points: number = 0;
+
+    word
+      .split("")
+      .forEach((letter) => (points += newScoreKey[letter.toLowerCase()]));
+
+    return points;
   },
-  {
-    name: "Simple Score",
-    description: "Each letter is worth 1 point.",
-    scoreFunction: (word: string): number => word.replace(" ", "").length,
-  },
-  {
-    name: "Bonus Vowels",
-    description: "Vowels are 3 pts, consonants are 1pt.",
-    scoreFunction: (word: string): number => {
-      let points: number = 0;
+};
+const simpleScoreAlgorithm: ScoringAlgorithm =   {
+  name: "Simple Score",
+  description: "Each letter is worth 1 point.",
+  scoreFunction: (word: string): number => word.replace(" ", "").length,
+};
+const vowelBonusScoreAlgorithm: ScoringAlgorithm = {
+  name: "Bonus Vowels",
+  description: "Vowels are 3 pts, consonants are 1pt.",
+  scoreFunction: (word: string): number => {
+    let points: number = 0;
 
-      word.split("").forEach((char) => (points += isVowel(char)));
+    word
+      .split("")
+      .forEach(char => (points += scoreVowel(char)));
 
-      return points;
-    },
+    return points;
   },
+};
+
+export const scoringAlgorithms:  ScoringAlgorithm[] = [
+  scrabbleAlgorithm,
+  simpleScoreAlgorithm,
+  vowelBonusScoreAlgorithm,
 ];
