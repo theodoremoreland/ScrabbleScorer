f3ac8457b5056f05a469fccdffd844d0dc28468d
theodoremoreland <theodoremoreland@yahoo.com>
2024-05-26 13:25:06 -0500

    implemented react query and usestate hooks for test

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,14 +1,32 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 
 // Third party
 import { useQuery } from '@tanstack/react-query';
+
+// Custom
+import { ScoringAlgorithmName } from './types/types';
+import getScore from './http/getScore';
+import getScoringAlgorithms from './http/getScoringAlgorithms';
 
 // Styles
 import './App.css';
 
 const App = (): ReactElement => {
+  const [word, setWord] = useState<string>('Test');
+  const [scoringAlgorithmName] = useState<ScoringAlgorithmName>(ScoringAlgorithmName.Scrabble);
+
+  const { data: scoreData } = useQuery({ queryKey: ['score', word, scoringAlgorithmName], queryFn: () => getScore({ word, scoringAlgorithmName }), enabled: Boolean(word)});
+  const { data: scoringAlgorithmsData } = useQuery({queryKey: ['scoringAlgorithms'], queryFn: getScoringAlgorithms});
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <div className="App">
+      {scoringAlgorithmsData && <span>Scoring Algorithm: {JSON.stringify(scoringAlgorithmsData)}</span>}
+      <input type="text" value={word} onChange={(e) => setWord(e.target.value)} />
+      {scoreData && <span>{scoreData.score}</span>}
     </div>
   )
 }
